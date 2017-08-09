6bb8f968a3985a504f7a110bb8d949f0930de827
Anna <post-a22@mail.ru>
2017-08-09 22:25:41 +0300

    Add project

---
+++ gulpfile.js
@@ -0,0 +1,100 @@
+"use strict";
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var rename = require("gulp-rename");
+var minify = require("gulp-csso");
+var svgstore = require("gulp-svgstore");
+var svgmin = require("gulp-svgmin");
+var server = require("browser-sync").create();
+var mqpacker = require("css-mqpacker");
+var imagemin = require("gulp-imagemin");
+var del = require("del");
+var run = require("run-sequence");
+
+gulp.task("style", function () {
+  gulp.src("sass/style.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer({
+        browsers: [
+          "last 2 versions"
+        ]
+      })
+    ]))
+    .pipe(gulp.dest("css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("css"))
+    .pipe(server.stream());
+});
+gulp.task("symbols", function () {
+  return gulp.src("img/icons/*.svg")
+    .pipe(svgmin())
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("symbols.svg"))
+    .pipe(gulp.dest("img"));
+});
+gulp.task("images", function () {
+  return gulp.src("img/**/*.{png,jpg,gif}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true})
+    ]))
+    .pipe(gulp.dest("img"));
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "fonts/**/*.{woff,woff2}",
+    "img/**",
+    "js/**",
+    "*.html"
+  ], {
+    base: "."
+  })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("html:copy", function () {
+  return gulp.src("*.html")
+    .pipe(gulp.dest("build"));
+});
+gulp.task("html:update", ["html:copy"], function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task("serve", ["style"], function () {
+  server.init({
+    server: ".",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("sass/**/*.{scss,sass}", ["style"]);
+  gulp.watch("*.html").on("change", server.reload);
+});
+
+gulp.task("build", function (fn) {
+  run(
+    "style",
+    "images",
+    "symbols",
+    "clean",
+    "copy",
+    fn
+  );
+});
